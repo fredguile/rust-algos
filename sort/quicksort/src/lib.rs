b7258b16f2554b4d03d62791828d4732d2e1c835
Fred Ghilini <fghilini@atlassian.com>
2021-07-13 20:27:03 +1000

    Added quicksort; test failing

---
+++ sort/quicksort/src/lib.rs
@@ -0,0 +1,51 @@
+pub fn quicksort(arr: &mut [i32]) {
+    if arr.len() <= 2 {
+        return;
+    }
+
+    let pivot = arr[arr.len() / 2];
+
+    let index = partition(arr, pivot);
+    quicksort(&mut arr[..index]);
+    quicksort(&mut arr[index + 1..]);
+}
+
+fn partition(arr: &mut [i32], pivot: i32) -> usize {
+    let mut i = 0usize;
+    let mut j = arr.len() - 1;
+
+    loop {
+        while arr[i] <= pivot {
+            i += 1;
+        }
+
+        while arr[j] > pivot {
+            j -= 1;
+        }
+
+        if i >= j {
+            break j;
+        }
+
+        arr.swap(i, j);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::quicksort;
+
+    #[test]
+    fn it_sorts_1() {
+        let mut arr = vec![4, 5, 3, 55, 7, 23, 54, 7];
+        quicksort(&mut arr);
+        assert_eq!(&arr[..], [3, 4, 5, 7, 7, 23, 54, 55]);
+    }
+
+    #[test]
+    fn it_sorts_2() {
+        let mut arr = vec![16, 15, 22, 100, 1, 7, 3, 100];
+        quicksort(&mut arr);
+        assert_eq!(&arr[..], [1, 3, 7, 15, 16, 22, 100, 100]);
+    }
+}
