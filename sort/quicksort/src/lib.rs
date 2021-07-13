059c35a885361beb8f67db722a6140e3e0696bd7
Fred Ghilini <fredguile@gmail.com>
2021-07-13 10:13:49 +1000

    Fixed odd case and tests

--- sort/quicksort/src/lib.rs
+++ sort/quicksort/src/lib.rs
@@ -1,5 +1,5 @@
 pub fn quicksort(arr: &mut [i32]) {
-    if arr.len() <= 2 {
+    if arr.len() < 3 {
         return;
     }
 
@@ -7,7 +7,7 @@ pub fn quicksort(arr: &mut [i32]) {
 
     let index = partition(arr, pivot);
     quicksort(&mut arr[..index]);
-    quicksort(&mut arr[index + 1..]);
+    quicksort(&mut arr[index..]);
 }
 
 fn partition(arr: &mut [i32], pivot: i32) -> usize {
@@ -15,7 +15,7 @@ fn partition(arr: &mut [i32], pivot: i32) -> usize {
     let mut j = arr.len() - 1;
 
     loop {
-        while arr[i] <= pivot {
+        while arr[i] < pivot {
             i += 1;
         }
 
@@ -23,7 +23,7 @@ fn partition(arr: &mut [i32], pivot: i32) -> usize {
             j -= 1;
         }
 
-        if i >= j {
+        if i >= j || arr[i] == arr[j] {
             break j;
         }
 
@@ -37,13 +37,20 @@ mod tests {
 
     #[test]
     fn it_sorts_1() {
+        let mut arr = vec![15, 3, 7];
+        quicksort(&mut arr);
+        assert_eq!(&arr[..], [3, 7, 15]);
+    }
+
+    #[test]
+    fn it_sorts_2() {
         let mut arr = vec![4, 5, 3, 55, 7, 23, 54, 7];
         quicksort(&mut arr);
         assert_eq!(&arr[..], [3, 4, 5, 7, 7, 23, 54, 55]);
     }
 
     #[test]
-    fn it_sorts_2() {
+    fn it_sorts_3() {
         let mut arr = vec![16, 15, 22, 100, 1, 7, 3, 100];
         quicksort(&mut arr);
         assert_eq!(&arr[..], [1, 3, 7, 15, 16, 22, 100, 100]);
