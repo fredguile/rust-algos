8081ddfc8a4e3835551e5ca09906e954fa46897a
Fred Ghilini <fredguile@gmail.com>
2021-07-24 00:52:18 +1000

    Added bubblesort

---
+++ sort/bubblesort/src/lib.rs
@@ -0,0 +1,48 @@
+pub fn bubblesort(arr: &mut [i32]) {
+    loop {
+        let mut swapped = false;
+
+        for i in 1..arr.len() {
+            if arr[i - 1] > arr[i] {
+                arr.swap(i - 1, i);
+                swapped = true;
+            }
+        }
+
+        if !swapped {
+            break;
+        }
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use rand::distributions::Uniform;
+    use rand::{self, thread_rng, Rng};
+
+    use super::bubblesort;
+
+    #[test]
+    pub fn bubblesort_works() {
+        let rng = &mut thread_rng();
+
+        for _ in 0..5000 {
+            let len = rng.gen::<usize>() % 32;
+            let mut v = rng
+                .sample_iter(Uniform::new(0, 100))
+                .take(len + 1)
+                .collect::<Vec<i32>>();
+            let original = &v.to_owned();
+
+            bubblesort(&mut v);
+            for i in 0..v.len() - 1 {
+                assert!(
+                    v[i] <= v[i + 1],
+                    "array not sorted: {:?} got instead: {:?}",
+                    &original,
+                    &v
+                );
+            }
+        }
+    }
+}
